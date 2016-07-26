21d24e935b6d3c1fb26074747098a1e32c897c1d
Hudson Burns Prestidge & Martin <pair+hudson-prestidge.martybutts@devbootcamp.com>
2016-07-27 11:03:35 +1200

    add lifeStories and rendered them

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var catsObj = {
  cats: [
-  {id: 1, name: 'fluffy'},
-  {id: 2, name: 'tick'}
+  {id: 1, name: 'Fluffy', image: 'https://i.ytimg.com/vi/0FEYvKxCnYw/maxresdefault.jpg', lifeStory: "I'm up to life 7"},
+  {id: 2, name: 'Tick', image: 'https://s-media-cache-ak0.pinimg.com/236x/59/77/cc/5977cc1ff2e1c6ebfc5a5e3014f62efb.jpg', lifeStory: 'Lived a life of luxury'}
  ]
 }
 
@@ -34,7 +34,8 @@ app.get('/cats/new', function(req, res) {
 })
 
 app.get('/cats/:id', function(req,res){
-  res.render('catsShow', catsObj.cats[0]); // try going to /cats/1
+  res.render('catsShow', catsObj.cats[req.params.id -1]);
+ console.log(req.params.id) // try going to /cats/1
 })
 
 app.post('/cats', function(req,res) {
