8640db8b2d8034aac0a9f96db5d53233d48aef20
Cameron Shotter & Dylan <pair+cam-shotter.dylanhul@devbootcamp.com>
2016-07-27 18:00:28 +1200

    Wrote to Jsonfile

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var fs = require('fs')
+var catsObj = require('./data/catsObj.json')
 
 var app = express();
 
@@ -14,19 +16,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //---------------------Ignore above here-------------------//
 
-var catsObj = {
- cats: [
-  {id: 1, name: 'fluffy', image: "https://pbs.twimg.com/profile_images/706844157093027840/2Aan_aSU.jpg", life_story: 'I am fluffy'},
-  {id: 2, name: 'tick' , image: "https://pbs.twimg.com/profile_images/706844157093027840/2Aan_aSU.jpg", life_story: 'I have ticks'},
-  {id: 4, name: 'business cat' , image: "https://pbs.twimg.com/profile_images/706844157093027840/2Aan_aSU.jpg", life_story: 'I am a cat'}
- ]
-}
+
+
+// var catsObj = {
+//  cats: [
+//   {id: 1, name: 'fluffy', image: "https://pbs.twimg.com/profile_images/706844157093027840/2Aan_aSU.jpg", life_story: 'I am fluffy'},
+//   {id: 2, name: 'tick' , image: "https://pbs.twimg.com/profile_images/706844157093027840/2Aan_aSU.jpg", life_story: 'I have ticks'},
+//   {id: 3, name: 'business cat' , image: "https://pbs.twimg.com/profile_images/706844157093027840/2Aan_aSU.jpg", life_story: 'I am a cat'}
+//  ]
+// }
 var catsLength = catsObj.cats.length
 var idCount = catsObj.cats[catsLength-1].id
-
-
-console.log("id count", idCount)
-console.log("cats obj ", catsLength)
 
 app.get('/', function(req, res) {
  res.redirect('/cats') // what is this doing?
@@ -44,18 +44,34 @@ app.get('/cats/:id', function(req,res){
   var catsFilter = catsObj.cats.filter(function(cat){
     return cat.id == req.params.id
   })
-  console.log("Cats Filterd", catsFilter)
+
   res.render("catsShow", catsFilter[0])
 })
+
 
 app.post('/cats', function(req,res) {
   var newObj = req.body
   newObj.id = idCount+1
+  console.log("this is req body", req.body)
   catsObj.cats.push(newObj)
-  res.send(catsObj)
+  writeAsync()
   res.render('catsIndex', catsObj)
 
-  console.log("this is req query ", req, "htis is res query ", res);
+  function writeAsync() {
+    var catsFile = path.join(__dirname, 'data/catsObj.json')
+    var catsString = JSON.stringify(catsObj, null, 2)
+    fs.writeFile(catsFile, catsString, function(err) {
+      if (!err) {
+        verifyExists(catsString)
+      }
+    })
+  }
+
+  function verifyExists (catsString) {
+    fs.exists(catsString, function(exists) {
+      console.log(catsString, 'exists:', exists)
+    })
+  }
 })
 
 module.exports = app;
