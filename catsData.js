35d3a2c142d17d95cd0fc73a8f30d95b92a33b36
Max Robinson & Mandy Baker <pair+Maxwell-Robinson.mandy-kate@devbootcamp.com>
2016-07-27 12:20:39 +1200

    added images

--- catsData.js
+++ catsData.js
@@ -1,10 +1,13 @@
 module.exports = {
  cats: [
-  {id: 1, name: 'fluffy'},
-  {id: 2, name: 'tick'}
+  {id: 1,
+  name: 'fluffy',
+  imageUrl:'http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg',
+  lifeStory: "lifestorytext"},
+
+  {id: 2,
+  name: 'tick',
+  imageUrl: 'https://i.ytimg.com/vi/cNycdfFEgBc/maxresdefault.jpg',
+  lifeStory: 'lifestorytext lifestroy text'}
  ]
 }
-
-
-
-// image: '' , lifeStory: ''
